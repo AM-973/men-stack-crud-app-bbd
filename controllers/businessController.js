0492ac551fce7cde32991b9ea8b799ab76db64d6
“AM-973” <AlMatar973@gmail.com>
2025-07-14 14:15:51 +0300

    Render business index added css fixed typos

---
+++ controllers/businessController.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+const Business = require('../models/business')
+
+// INDEX OF ALL BUSINESSES
+router.get('/', async (req, res) => {
+    const allBusinesses = await Business.find()
+    console.log('allBusinesses:', allBusinesses)
+    res.render('businesses/index.ejs', {allBusinesses: allBusinesses})
+})
+
+// RENDER NEW BUSINESS FORM
+router.get('/new', (req, res) => {
+    res.render('businesses/new.ejs')
+})
+
+// POST FORM DATA TO DATABASE
+router.post('/', async (req, res) => {
+    if (req.body.isVerified === 'on') {
+        req.body.isVerified = true
+    } else {
+        req.body.isVerified = false
+    }
+    console.log(req.body)
+    await Business.create(req.body)
+    res.redirect('/businesses/')
+})
+
+
+module.exports = router
