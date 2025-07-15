2fac6240131087ea96f4b659f72f178342bdb376
“AM-973” <AlMatar973@gmail.com>
2025-07-15 13:28:02 +0300

    final deliverables except recap

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const Business = require('../models/business')
+// everything in this file has /businesses front
 
 // INDEX OF ALL BUSINESSES
 router.get('/', async (req, res) => {
     const allBusinesses = await Business.find()
-    console.log('allBusinesses:', allBusinesses)
-    res.render('businesses/index.ejs', {allBusinesses: allBusinesses})
+    res.render('businesses/index.ejs', { allBusinesses: allBusinesses })
 })
 
 // RENDER NEW BUSINESS FORM
@@ -26,11 +26,32 @@ router.post('/', async (req, res) => {
     res.redirect('/businesses/')
 })
 
-
 // SHOW ONE BUSINESS
-router.get('/:businessId', async (req, res)=> {
+router.get('/:businessId', async (req, res) => {
     const foundBusiness = await Business.findById(req.params.businessId)
-    res.render('businesses/show.ejs', {foundBusiness: foundBusiness})
+    res.render('businesses/show.ejs', { foundBusiness: foundBusiness })
 })
 
+router.delete('/:businessId', async (req, res) => {
+    await Business.findByIdAndDelete(req.params.businessId)
+    res.redirect('/businesses')
+})
+
+// GET /businesses/:businessId/edit
+router.get('/:businessId/edit', async (req, res) => {
+    const foundBusiness = await Business.findById(req.params.businessId)
+    res.render('businesses/edit.ejs', {foundBusiness: foundBusiness})
+})
+// controller function should render 'businesses/edit.ejs' <--- ejs file should have edit form
+// PUT for submitting the form 
+router.put('/:businessId', async (req, res)=> {
+    if (req.body.isVerified === 'on') {
+        req.body.isVerified = true
+    } else {
+        req.body.isVerified = false
+    }
+    console.log(req.body)
+    await Business.findByIdAndUpdate(req.params.businessId, req.body)
+    res.redirect(`/businesses/${req.params.businessId}`)
+})
 module.exports = router
